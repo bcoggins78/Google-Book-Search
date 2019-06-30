77d98668a78a8b28e24febb656e9947247575391
bcoggins78 <bcoggins78@gmail.com>
2019-06-30 15:17:45 -0400

    Added required modules and configured connection to DB

--- server.js
+++ server.js
@@ -1,18 +1,33 @@
 const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+app.use(logger("dev"));
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Location of api routes
+var routes = require("./routes/api-routes.js");
+app.use(routes);
+
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+// MongoDB connection parameters
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooks";
+mongoose.connect(MONGODB_URI);
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
