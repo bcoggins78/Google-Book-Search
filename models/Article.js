6556ae4d9a072346bc9d6ab1a4e8f4573664c533
bcoggins78 <bcoggins78@gmail.com>
2019-06-30 15:18:06 -0400

    Defined DB model for Book

---
+++ models/Article.js
@@ -0,0 +1,38 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var BookSchema = new Schema({
+  
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  description: {
+      type: String,
+      required: true
+  },
+  image: {
+      type: String,
+      required: true
+  },
+  link: {
+      type: String,
+      required: true
+  },
+  saved: {
+      type: Boolean,
+      default: false
+  }
+});
+
+
+var Book = mongoose.model("Book", BookSchema);
+
+
+module.exports = Book;
