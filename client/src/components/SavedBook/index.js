5219392c784ec9516aa354525cd3ea0ee98d571c
bcoggins78 <bcoggins78@gmail.com>
2019-07-04 22:20:44 -0400

    Updated so the content better fits the card

--- client/src/components/SavedBook/index.js
+++ client/src/components/SavedBook/index.js
@@ -1,34 +1,31 @@
 import React from "react";
+import "./style.css"
 
 function SavedBook(props) {
     
     return (
-        <div className="row">
+        <div className="row w-100">
 
-            <div className="card-group">
-
-
-                <div className="card col-2">
-                    <a href={props.link} target="_blank" rel="noopener noreferrer"><img className="card-image" src={props.image} alt={props.title} /></a>
-                        
-                        
-                    
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-12 mt-3">
+                        <div className="card">
+                            <div className="card-horizontal">
+                                <div className="img-square-wrapper">
+                                    <a href={props.link} target="_blank" rel="noopener noreferrer"><img className="" src={props.image} alt={props.title} title={props.title}/></a>
+                                </div>
+                                <div className="card-body">
+                                    <h4 className="card-title">{props.title}</h4>
+                                    <p className="card-text">{props.description}</p>
+                                </div>
+                            </div>
+                            <div className="card-footer">
+                                <small className="text-muted">Author: {props.authors ? props.authors.join(', '): 'n/a'}</small>
+                                <button type="button" onClick={() => props.onSelect()} className="btn btn-danger float-right">Delete</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="card col-10">
-                    <div className="card-body">
-                        <h4 className="card-title">{props.title}</h4>
-                        <p className="card-text">{props.description}</p>
-                    </div>
-                    <div className="card-footer">
-                        Author: {props.authors}
-                    </div>
-                    <div>
-                    <button type="button" onClick={() => props.onSelect()} className="btn btn-primary">Delete</button>
-                    </div>
-                </div>
-                
-
-
             </div>
         </div>
     );
